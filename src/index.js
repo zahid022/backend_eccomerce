293bd27f37046daf99ac6a045243fded5ea41a28
zahid022 <zahid.hzade001@gmail.com>
2024-11-15 01:44:07 +0400

    fgdfgdsfds

--- src/index.js
+++ src/index.js
@@ -6,22 +6,20 @@ const brandRouter = require("./routes/brand.route");
 const categoryRouter = require("./routes/category.route");
 const productRouter = require("./routes/product.route");
 const loginRouter = require("./routes/login.route");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Test endpoint
-app.get("/api", (req, res) => {
-  res.send({ message: "application is running" });
+app.get("/", (req, res) => {
+    res.send({ message: "Application is running" });
 });
 
-// Rotalarınızı tanımlayın (base path `/api` olacak şekilde)
-app.use("/api/brand", brandRouter);
-app.use("/api/category", categoryRouter);
-app.use("/api/products", productRouter);
-app.use("/api/login", loginRouter);
+app.use("/products", productRouter);
+app.use("/brands", brandRouter);
+app.use("/category", categoryRouter);
+app.use("/login", loginRouter);
 
-// `app.listen()` KULLANMAYIN
 module.exports = app;
